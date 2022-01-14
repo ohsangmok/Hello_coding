d37417e055e70cbcffe3b05c1038c60b4e61ec66
ohsangmok <96159092+ohsangmok@users.noreply.github.com>
2022-01-14 12:25:59 +0900

    Update Hello_code.cs

--- Hello_coding/Assets/Hello_code.cs
+++ Hello_coding/Assets/Hello_code.cs
@@ -7,7 +7,7 @@ public class Hello_code : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world");
+        Debug.Log("Hello world!");
     }
 
     // Update is called once per frame
