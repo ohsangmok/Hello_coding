537ff0dedbce0c46bc184e6fcc2ab545aa34f056
ohsangmok <96159092+ohsangmok@users.noreply.github.com>
2022-01-14 12:28:28 +0900

    Update Hello_code.cs

--- Hello_coding/Assets/Hello_code.cs
+++ Hello_coding/Assets/Hello_code.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hello_code : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Hello world!");
-    }
+public class Hello_code : MonoBehaviour { 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    void Start() { 
+        Debug.Log("Hello world!");
     }
 }
