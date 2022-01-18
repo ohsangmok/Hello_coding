18de0bd1028e7ffbf4151af5c30ea008451032a2
ohsangmok <96159092+ohsangmok@users.noreply.github.com>
2022-01-18 12:42:41 +0900

    22-01-18
    u_u

--- Hello_coding/Assets/Hello_code.cs
+++ Hello_coding/Assets/Hello_code.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hello_code : MonoBehaviour { 
+public class Hello_code : MonoBehaviour {
 
-    
-    void Start() { 
-        Debug.Log("Hello world!");
+
+    void Start()
+    {
+        int[] students = new int[5];
+
+        students[0] = 100;
+        students[1] = 90;
+        students[2] = 80;
+        students[3] = 70;
+        students[4] = 60;
+
+        Debug.Log("0번 학생의 점수 : " + students[0]);
+        Debug.Log("1번 학생의 점수 : " + students[1]);
+        Debug.Log("2번 학생의 점수 : " + students[2]);
+        Debug.Log("3번 학생의 점수 : " + students[3]);
+        Debug.Log("4번 학생의 점수 : " + students[4]);
     }
+       
 }
